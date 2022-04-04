71f24ca127611241986fb6ca87e024c799219f51
LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>
2022-04-04 11:12:16 -0600

    new direction
    still doesnt work but taking a new approach at least

--- PuzzleController.java
+++ PuzzleController.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.Pane;
 public class PuzzleController {
 	@FXML
 	Pane red, yellow, green, blue;
+	Pane[] ref = new Pane[] {red, yellow, green, blue};
+	Pane[] colors = new Pane[4];
 	char[][] puzzles = new char[][] {
 		{'R', 'Y', 'G', 'B'},
 		{'B', 'G', 'Y', 'R'},
@@ -23,36 +25,29 @@ public class PuzzleController {
 	protected void onStart() {
 		Random rand = new Random();
 		int puzzleInt = rand.nextInt((3 - 0) + 1) + 0;
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				if(puzzles[puzzleInt][j] == puzzles[0][i]) {
+					colors[j] = ref[i];
+				}
+			}
+		}
 		try {
-			switch(puzzles[puzzleInt][0]) {
-				case 'R':
-				System.out.println("change red");
-				red.setStyle("-fx-border-color: red; -fx-border-width: 5px; -fx-background-color: red; -fx-background-insets: 5;");
-				Thread.sleep(2500);
-				System.out.println("wake up");
-				red.setStyle("-fx-background-color: red; -fx-background-insets: 5;");
-				break;
-				case 'Y':
-				System.out.println("change yellow");
-				yellow.setStyle("-fx-border-color: yellow; -fx-border-width: 5px; -fx-background-color: yellow; -fx-background-insets: 5;");
-				Thread.sleep(2500);
-				System.out.println("wake up");
-				yellow.setStyle("-fx-background-color: yellow; -fx-background-insets: 5;");
-				break;
-				case 'G':
-				System.out.println("change green");
-				green.setStyle("-fx-border-color: green; -fx-border-width: 5px; -fx-background-color: green; -fx-background-insets: 5;");
-				Thread.sleep(2500);
-				System.out.println("wake up");
-				green.setStyle("-fx-background-color: green; -fx-background-insets: 5;");
-				break;
-				case 'B':
-				System.out.println("change blue");
-				blue.setStyle("-fx-border-color: blue; -fx-border-width: 5px; -fx-background-color: blue; -fx-background-insets: 5;");
-				Thread.sleep(2500);
-				System.out.println("wake up");
-				blue.setStyle("-fx-background-color: blue; -fx-background-insets: 5;");
-				break;
+			for(int i = 0; i < 4; i++) {
+				System.out.println("run");
+				if(colors[i] == red) {
+					red.setStyle("-fx-border-color: red; -fx-border-width: 5px; -fx-background-color: red; -fx-background-insets: 5;");
+					Thread.sleep(2500);
+				} else if(colors[i] == yellow) {
+					yellow.setStyle("-fx-border-color: yellow; -fx-border-width: 5px; -fx-background-color: yellow; -fx-background-insets: 5;");
+					Thread.sleep(2500);
+				} else if(colors[i] == green) {
+					green.setStyle("-fx-border-color: green; -fx-border-width: 5px; -fx-background-color: green; -fx-background-insets: 5;");
+					Thread.sleep(2500);
+				} else {
+					blue.setStyle("-fx-border-color: blue; -fx-border-width: 5px; -fx-background-color: blue; -fx-background-insets: 5;");
+					Thread.sleep(2500);
+				}
 			}
 		} catch(InterruptedException ex) {
 			System.out.println(ex.toString());
