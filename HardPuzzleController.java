c0faecb919c5b41b3900df37f2c5b766c4218589
LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>
2022-04-21 10:34:59 -0600

    colors work now almost

--- HardPuzzleController.java
+++ HardPuzzleController.java
@@ -16,20 +16,45 @@ import javafx.scene.paint.Color;
 public class HardPuzzleController {
 	@FXML
 	Pane aaa, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo, pp, qq, rr, ss, tt, uu, vv, ww, xx, yy, zz;
-	Pane[] blocks = {aaa, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo, pp, qq, rr, ss, tt, uu, vv, ww, xx, yy, zz};
+	@FXML
+	List<Pane> blocks;
+	@FXML
+	Button start;
 	String[] colors = {"black; ", "green; ", "blue; ", "red; ", "yellow; ", "orange; "};
-	//Color[] colors = {Color.BLACK, Color.GREEN, Color.BLUE, Color.RED, Color.YELLOW, Color.ORANGE};
 	Random random = new Random();
+	int redCount = 0;
+	List<Pane> redBlocks;
 	
 	@FXML
 	protected void onStart() {
-		for(int i = 0; i < blocks.length; i++) {
+		for(int i = 0; i < blocks.size(); i++) {
 			int color = random.nextInt(5);
-			System.out.println(color);
+			if(colors[color].equals("red; ")) {
+				redCount++;
+				//redBlocks.add(blocks.get(i));
+			}
 			String style = "-fx-background-color: " + colors[color] + "-fx-border-color: black;";
-			System.out.println(style);
-			System.out.println(blocks[i]);
-			blocks[i].setStyle(style);
+			blocks.get(i).setStyle(style);
+			start.setDisable(true);
+		}
+		System.out.println(redCount);
+	}
+	@FXML
+	protected void onClick(MouseEvent event) {
+		Pane pane = (Pane) event.getTarget();
+		if(redBlocks.contains(pane)) {
+			Line one = new Line(10, 10, pane.getWidth() - 10, pane.getHeight() - 10);
+			one.endXProperty().bind(pane.widthProperty().subtract(10));
+			one.endYProperty().bind(pane.heightProperty().subtract(10));
+			Line two = new Line(10, pane.getHeight() - 10, pane.getWidth() - 10, 10);
+			two.startYProperty().bind(pane.heightProperty().subtract(10));
+			two.endXProperty().bind(pane.widthProperty().subtract(10));
+			pane.getChildren().addAll(one, two);
+			redCount--;
+			pane.setDisable(true);
+		}
+		else {
+			return;
 		}
 	}
 }
