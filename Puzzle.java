a79d5e82a74a49e78ade5f29e953a79a6dac2c39
LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>
2022-04-25 09:20:35 -0600

    alarm has sound now

--- Puzzle.java
+++ Puzzle.java
@@ -2,6 +2,10 @@ import javafx.application.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.stage.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import java.io.File;
 
 public class Puzzle extends Application {
 	@Override
@@ -10,6 +14,12 @@ public class Puzzle extends Application {
 		primaryStage.setTitle("Puzzle");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
+		String musicFile = "alarmsound.wav";
+		Media sound = new Media(new File(musicFile).toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.setAutoPlay(true);
+		mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+		mediaPlayer.play();
 	}
 	
 	public static void main(String[] args) {
